cc47b8763ca55d1b9ffbe188508e1b1c1658c690
Alex Norton <alex.norton@bbc.co.uk>
2015-09-25 10:23:45 +0100

    aws error handling

--- storage/s3.go
+++ storage/s3.go
@@ -5,12 +5,13 @@ import (
 	"os"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 type Storage interface {
-	Get(key string) (*s3.GetObjectOutput, error)
-	Set(key string, data []byte) (*s3.PutObjectOutput, error)
+	Get(key string) (*s3.GetObjectOutput, *Error)
+	Set(key string, data []byte) (*s3.PutObjectOutput, *Error)
 }
 
 type S3Storage struct {
@@ -35,18 +36,43 @@ func (s *S3Storage) getService() *s3.S3 {
 	return s.service
 }
 
-func (s *S3Storage) Get(key string) (*s3.GetObjectOutput, error) {
-	return s.getService().GetObject(&s3.GetObjectInput{
+func (s *S3Storage) Get(key string) (*s3.GetObjectOutput, *Error) {
+	response, err := s.getService().GetObject(&s3.GetObjectInput{
 		Bucket: aws.String(os.Getenv("S3_BUCKET")),
 		Key:    aws.String(key),
 	})
+	return response, handleError(err)
 }
 
-func (s *S3Storage) Set(key string, data []byte) (*s3.PutObjectOutput, error) {
-	return s.getService().PutObject(&s3.PutObjectInput{
+func (s *S3Storage) Set(key string, data []byte) (*s3.PutObjectOutput, *Error) {
+	response, err := s.getService().PutObject(&s3.PutObjectInput{
 		Bucket:      aws.String(os.Getenv("S3_BUCKET")),
 		Key:         aws.String(key),
 		Body:        bytes.NewReader(data),
 		ContentType: aws.String("application/json"),
 	})
+	return response, handleError(err)
+}
+
+func handleError(err error) *Error {
+	var storageError *Error
+
+	if awsErr, ok := err.(awserr.Error); ok {
+		storageError = &Error{}
+		storageError.Message = awsErr.Code() + ": " + awsErr.Message()
+		if reqErr, ok := err.(awserr.RequestFailure); ok {
+			storageError.Status = reqErr.StatusCode()
+		}
+	}
+
+	return storageError
+}
+
+type Error struct {
+	Message string
+	Status  int
+}
+
+func (e *Error) Error() string {
+	return e.Message
 }
