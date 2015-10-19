0e13e420f4c87b7cc08865f3d983f74b96cd0998
Dan Arnould <dan@arnould.co.uk>
2015-10-19 18:34:59 +0100

    Replace redis implementation with redigo

--- caching/redis_cache.go
+++ caching/redis_cache.go
@@ -1,34 +1,55 @@
 package caching
 
 import (
+	"fmt"
 	"os"
 	"time"
 
-	"github.com/bbc/mozart-api-common/Godeps/_workspace/src/gopkg.in/redis.v3"
+	"github.com/garyburd/redigo/redis"
 )
 
 type RedisCache struct {
-	client *redis.Client
+	pool *redis.Pool
 }
 
-func (c *RedisCache) getClient() *redis.Client {
-	if c.client == nil {
-		c.client = redis.NewClient(&redis.Options{
-			Addr:        os.Getenv("REDIS_HOST"),
-			Password:    "", // no password set
-			DB:          0,  // use default DB
-			DialTimeout: 3 * time.Second,
-			PoolSize:    300,
-		})
+func (c *RedisCache) getConn() redis.Conn {
+	if c.pool == nil {
+		c.pool = &redis.Pool{
+			MaxIdle: 30,
+			Dial: func() (redis.Conn, error) {
+				c, err := redis.Dial("tcp", os.Getenv("REDIS_HOST"))
+				if err != nil {
+					return nil, err
+				}
+				return c, nil
+			},
+		}
 	}
 
-	return c.client
+	return c.pool.Get()
 }
 
 func (c *RedisCache) Get(key string) (string, error) {
-	return c.getClient().Get(key).Result()
+	conn := c.getConn()
+	defer conn.Close()
+
+	val, err := redis.String(conn.Do("GET", key))
+	if err != nil {
+		return "", fmt.Errorf("can't get redis key %q: %s", key, err)
+	}
+
+	return val, nil
 }
 
 func (c *RedisCache) Set(key string, data string, expiration time.Duration) (string, error) {
-	return c.getClient().Set(key, data, expiration).Result()
+	conn := c.getConn()
+	defer conn.Close()
+
+	val, err := redis.String(conn.Do("SET", key, data, expiration))
+	if err != nil {
+		return "", fmt.Errorf("can't set redis key %q: %s", key, err)
+	}
+
+	return val, nil
+
 }
