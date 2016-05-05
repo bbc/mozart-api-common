79021cd5d2141df30bf79c27e0b42728ed446594
Integralist <mark.mcdx@gmail.com>
2016-05-05 11:50:26 +0100

    Try explicit use of setting output location

--- logger/logger.go
+++ logger/logger.go
@@ -2,6 +2,7 @@ package logger
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/bbc/mozart-api-common/Godeps/_workspace/src/github.com/Sirupsen/logrus"
 )
@@ -10,11 +11,13 @@ var log = logrus.New()
 
 func init() {
 	log.Formatter = new(logrus.JSONFormatter)
+	log.Out = os.Stdout // this should be the default but if this fails I'll try os.Stderr
 
 	hook := NewStatsDHook()
 	log.Hooks.Add(hook)
 }
 
+// Log is a HTTP log wrapper abstraction
 func Log(inner http.Handler, name string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.WithFields(logrus.Fields{
@@ -25,26 +28,32 @@ func Log(inner http.Handler, name string) http.Handler {
 	})
 }
 
+// Info is a wrapper abstraction
 func Info(message map[string]interface{}) {
 	log.WithFields(message).Info()
 }
 
+// Error is a wrapper abstraction
 func Error(message map[string]interface{}) {
 	log.WithFields(message).Error("error")
 }
 
+// Warn is a wrapper abstraction
 func Warn(message map[string]interface{}) {
 	log.WithFields(message).Warn()
 }
 
+// Debug is a wrapper abstraction
 func Debug(message map[string]interface{}) {
 	log.WithFields(message).Debug()
 }
 
+// Panic is a wrapper abstraction
 func Panic(message map[string]interface{}) {
 	log.WithFields(message).Panic("panic")
 }
 
+// Fatal is a wrapper abstraction
 func Fatal(message map[string]interface{}) {
 	log.WithFields(message).Fatal("fatal")
 }
